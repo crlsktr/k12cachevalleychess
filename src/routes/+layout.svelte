0ad6abcc9742a3b247698b8aa67dfe4d8a89fd82
crlsktr <crlsktr@gmail.com>
2024-10-24 22:45:47 -0600

    add resources page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,11 +15,13 @@
 		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">
+				<strong class="m-4 text-xl uppercase">
 					<a href="{base}/"><svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#ffffff}</style><path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm64 64v64h64V96h64v64h64V96h64v64H320v64h64v64H320v64h64v64H320V352H256v64H192V352H128v64H64V352h64V288H64V224h64V160H64V96h64zm64 128h64V160H192v64zm0 64V224H128v64h64zm64 0H192v64h64V288zm0 0h64V224H256v64z"/></svg></a>
 				</strong>
+				<a class="p-4 block variant-ghost-primary" href="{base}/resources" rel="noreferrer">Resources</a>
 				
 			</svelte:fragment>
+			
 			<svelte:fragment slot="trail">
 				<a
 					class="btn btn-lg variant-filled-primary"
---
+++ src/routes/resources/+page.svelte
@@ -0,0 +1,12 @@
+<h2 class="h2 text-center">Additional Resources</h2>
+<div class="container mx-auto p-4">
+	<p class="h3 text-center">Check out these resources to help you prepare for the tournament!</p>
+	<div class="card p-4">
+		<iframe
+		title="Resources"
+		src="https://drive.google.com/embeddedfolderview?id=1csJye_tz3Pb0_43Gfj0hR3aQCg0BgHDf#grid"
+		style="width:100%; height:600px; border:0;"
+	/>
+	</div>
+</div>
+
