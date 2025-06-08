de05f5bfe1c0430736bdbf046fe43ee3358d78f1
crlsktr <crlsktr@gmail.com>
2025-06-07 22:43:40 -0600

    restart the project in skeleton v3

---
+++ old/src/routes/resources/+page.svelte
@@ -0,0 +1,9 @@
+<h2 class="h2 text-center">Additional Resources</h2>
+<div class="container mx-auto p-4">
+	<p class="h3 text-center">Check out these resources to help you prepare for the tournament!</p>
+</div>
+<iframe
+		title="Resources"
+		src="https://drive.google.com/embeddedfolderview?id=1csJye_tz3Pb0_43Gfj0hR3aQCg0BgHDf#list"
+		style="width: 100%; height:600px; border:0;"
+	/>
---
+++ old/src/routes/trainings/+page.svelte
@@ -0,0 +1,4 @@
+<h1>hello</h1>
+<div class="container mx-auto p-4">
+	<iframe title="Basics" class="w-max h-max" src="https://lichess.org/study/embed/qRdcltZp/oQZnCAuv" frameborder=0></iframe>
+</div>
