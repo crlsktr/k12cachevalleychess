062fe26bfa12d8e69af8d97b3f4d34202d49229c
crlsktr <crlsktr@gmail.com>
2025-06-11 12:22:37 -0600

    prepare for static adapter

--- cachevalleychess/src/routes/+page.svelte
+++ cachevalleychess/src/routes/+page.svelte
@@ -1,16 +1,4 @@
 <script lang="ts">
-	import { assets, base } from "$app/paths";
-	
+	import Home from './(about)/home/+page@(about).svelte'
 </script>
-
-
-<article class="bg-surface-100-900 text-center">
-	<header>
-	  <h2 class="h2 preset-glass-primary text-5xl">Cache Chess</h2>
-	  <span>Home of the Warren Pugh Chess Tournament</span>
-	<img src="{assets}/board.webp" class="aspect-auto w-full rounded-4xl" alt="banner" />
-	</header>
-	<p>
-		lorem ipsum
-	</p>
-</article>
+<Home></Home>
