d3b6bfb7940f213a6e25ee509feba6cb20909792
crlsktr <crlsktr@gmail.com>
2025-06-13 14:34:33 -0600

    mastodon feed

--- cachevalleychess/src/routes/(about)/home/+page@(about).svelte
+++ cachevalleychess/src/routes/(about)/home/+page@(about).svelte
@@ -1,16 +1,20 @@
 <script lang="ts">
 	import { assets, base } from "$app/paths";
-	
+	import Socialfeed from "$lib/socialfeed.svelte";
 </script>
 
-
-<article class="bg-surface-100-900 text-center">
-	<header>
-	  <h2 class="h2 preset-glass-primary text-5xl">Cache Chess</h2>
-	  <span>Home of the Warren Pugh Chess Tournament</span>
-	<img src="{assets}/board.webp" class="aspect-auto w-full rounded-4xl" alt="banner" />
+<article class="text-center">
+	<header class="flex justify-evenly p-4">
+		<div class="md:inline-block place-content-center ">
+			<h2 class="h1 preset-glass-primary md:align-middle">Cache Chess</h2>
+			<span>Home of the Warren Pugh Chess Tournament</span>
+		</div>
+		<img class="aspect-auto w-full md:max-w-1/4 md:inline rounded-4xl lg:self-center" alt="banner" src="{assets}/board.webp"/>
 	</header>
-	<p>
-		lorem ipsum
-	</p>
+	<div>
+	  <h2 class="h2 text-center">Bruce Bohm's Guide for Chess beginners</h2>
+	</div>
+	<div class="p-4 flex justify-center">
+		<Socialfeed></Socialfeed>
+	</div>
 </article>
--- cachevalleychess/src/routes/(sections)/+layout@.svelte
+++ cachevalleychess/src/routes/(sections)/+layout@.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
+	import Socialfeed from '$lib/socialfeed.svelte';
 	import '../../app.css'
 	let { children } = $props();
 </script>
 
 
 <!-- Page -->
-<div class="container mx-auto grid grid-cols-1 xl:grid-cols-[200px_minmax(0px,_1fr)_200px]"> 
+<div class="container mx-auto grid grid-cols-1 xl:grid-cols-[200px_minmax(0px,_1fr)_400px]"> 
 	<!-- Sidebar (Left) -->
 	<!-- NOTE: hidden in smaller screen sizes -->
 	<aside class="sticky top-0 col-span-1 hidden h-screen p-4 xl:block">(sidebar)</aside>
@@ -15,5 +16,7 @@
 	</main>
 	<!-- Sidebar (Right) -->
 	<!-- NOTE: hidden in smaller screen sizes -->
-	<aside class="sticky top-0 col-span-1 hidden h-screen p-4 xl:block">(news)</aside>
+	<aside class="sticky top-0 col-span-1 hidden h-screen p-4 xl:block">
+		<Socialfeed></Socialfeed>
+	</aside>
 </div>
