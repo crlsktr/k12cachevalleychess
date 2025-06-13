33797543dcc25e01bf469b83fae3db4dabdbdc5d
crlsktr <crlsktr@gmail.com>
2025-06-13 14:34:33 -0600

    mastodon feed

---
+++ cachevalleychess/src/lib/socialfeed.svelte
@@ -0,0 +1,24 @@
+
+
+<div class="card preset-filled-surface-100-900 border-[1px] border-surface-200-800 card-hover divide-surface-200-800 block max-w-md divide-y overflow-hidden">
+
+  <header>
+    <!-- <img src={imgSrc} class="aspect-[21/9] w-full grayscale hue-rotate-90" alt="banner" /> -->
+  </header>
+
+  <article class="space-y-4 p-4">
+    <!-- <a class="mastodon-feed"
+   href="https://mstdn.party/@cachechess"
+   data-toot-limit="4"
+   data-exclulde-replies="true"
+   data-exclude-reblogs="true"
+   >@cachechess in the fediverse</a> -->
+
+   <iframe title="news" allowfullscreen sandbox="allow-top-navigation allow-scripts allow-popups allow-popups-to-escape-sandbox" style="max-width:100vw;max-height:100vh;" width="100%" height="800" src="https://www.mastofeed.com/apiv2/feed?userurl=https%3A%2F%2Fmstdn.party%2Fusers%2Fcachechess&theme=auto&size=100&header=true&replies=false&boosts=false"></iframe>
+  </article>
+  <footer class="flex items-center justify-between gap-4 p-4">
+    <small class="opacity-60">Feed maintained by bruce[at]cachechess.com</small>
+    <small class="opacity-60">&#9812;&#9813;&#9815;</small>
+  </footer>
+  
+</div>
