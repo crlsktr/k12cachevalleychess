c83cc3c3dec91f8205c7209c488163a607b372dc
crlsktr <crlsktr@gmail.com>
2025-06-19 23:17:21 -0600

    setup github workflow

--- svelte.config.js
+++ svelte.config.js
@@ -2,9 +2,13 @@ import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+console.log("xkcd ------------------------> Deno.env.get(BASE_PATH)", Deno.env.get("BASE_PATH"));
+
 const config = {
 	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter() },
+	kit: { adapter: adapter(), paths: {
+		base: Deno.env.get("BASE_PATH") || ""
+	}},
 	extensions: ['.svelte', '.svx']
 };
 
